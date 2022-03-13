64ff41d254fe8b4bf63fec18333e89585ca277a8
Christopher Budd <cbudd@adventuregang.ca>
2022-03-13 19:20:47 -0400

    Added Java Classes Needed
    Added 4 classes
    Game
    GameRound
    Icon
    Setting

---
+++ app/src/main/java/com/example/myapplication/Game.java
@@ -0,0 +1,9 @@
+package com.example.myapplication;
+/*
+Stores high score
+Starts gameRound
+Has Game Over when gameRound ends
+ */
+public class Game {
+
+}
---
+++ app/src/main/java/com/example/myapplication/GameRound.java
@@ -0,0 +1,13 @@
+package com.example.myapplication;
+
+/*
+Game Round Object
+Use this to create the game
+Holds a list of all the icons
+Array to store order of icons
+randomly selects one of the five icons stored
+ */
+public class GameRound {
+    public GameRound() {
+    }
+}

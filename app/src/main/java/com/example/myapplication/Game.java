03d3c747fc1ed95ef0c986f54ca2ec415a70bc14
Christopher Budd <cbudd@adventuregang.ca>
2022-03-15 13:06:47 -0400

    Game v1 Create
    Added first functions

--- app/src/main/java/com/example/myapplication/Game.java
+++ app/src/main/java/com/example/myapplication/Game.java
@@ -5,5 +5,40 @@ Starts gameRound
 Has Game Over when gameRound ends
  */
 public class Game {
+    private GameRound gameRound;
+    private int orderNumber;
+    private int points;
+    private boolean gameOver;
+    public Game(){
+        gameRound = new GameRound();
+        orderNumber = 0;
+        gameOver = false;
+    }
+
+    public void newGameRound(){
+        gameRound = new GameRound();
+        orderNumber = 0;
+        gameOver = false;
+    }
+    /*
+    WHen button clicked checks if it is correct
+    Checks if the game
+     */
+    public void buttonClicked(String name){
+        boolean correctButton = gameRound.checkCorrect(name, orderNumber);
+        if (correctButton){
+
+            if (orderNumber == gameRound.getOrderLength()){
+                gameRound.nextLevel();
+                orderNumber =0;
+            }else{
+                orderNumber++;
+            }
+        }else{
+            gameOver = true;
+        }
+
+    }
+
 
 }
